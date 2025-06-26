17000de759f52a10b7a892c85362465d5e3850b8
wan <conut22@naver.com>
2025-06-26 12:40:07 +0900

    Revert "💄 style: add comments clarifying hook index flow"
    This reverts commit a9be81cba73e732e8b6903f0181fae571dadc9c7.

--- src/core/useState.js
+++ src/core/useState.js
@@ -10,7 +10,6 @@ export function useState(initialValue) {
     stateBucket[hookIndex] = initialValue;
   }
 
-  // #1 이 훅이 사용할 “슬롯 번호”를 고정(snapshot)
   const currentIndex = hookIndex;
 
   function setState(nextValue) {
@@ -21,12 +20,10 @@ export function useState(initialValue) {
       return; // 값이 동일하면 리렌더링 방지
     }
     stateBucket[currentIndex] = next;
-    reRender();
+    reRender(); // 전체 리랜더링 로직
   }
 
-  // #2. 다음 훅 호출을 위해 인덱스 증가
   state.hookIndex++;
 
-  // #1 을 참조하여 반환
   return [stateBucket[currentIndex], setState];
 }
